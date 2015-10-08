99a11f4041c239dd30de6ea43476cbfad27a05d7
dale <dale.guo@loweprofero.com>
2015-10-08 10:16:20 +0800

    mysql change

--- sites/default/settings.php
+++ sites/default/settings.php
@@ -205,7 +205,7 @@ $databases = array (
   array (
     'default' => 
     array (
-      'database' => 'jvgMmduFJsqxCqduQTxq',
+      'database' => 'qzfVYMXrozLlThdYuQrX',// wxnew /jvgMmduFJsqxCqduQTxq
       'username' => '3IgpsqmgbjHBbGZDUalZfkjV',
       'password' => 'nKzYxGM6aEqUNMsqMYrpG7mCZFe2jV6M',
       'host' => 'sqld.duapp.com',
