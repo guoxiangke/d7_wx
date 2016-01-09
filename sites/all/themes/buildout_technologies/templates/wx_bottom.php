ab340afd5317216a3f002f4b378fa3e81f0a06b0
dale <dale.guo@loweprofero.com>
2016-01-09 10:00:03 +0800

    r index

--- sites/all/themes/buildout_technologies/templates/wx_bottom.php
+++ sites/all/themes/buildout_technologies/templates/wx_bottom.php
@@ -1,5 +1,5 @@
 <div class="rich_media_tool">
-  <!-- <a class="media_tool_meta meta_primary" href="">阅读原文</a> -->
+  <a class="media_tool_meta meta_primary" href="/">回到首页</a>
   <div id="js_read_area" class="media_tool_meta tips_global meta_primary">阅读<span id="statistics_counter">1000+</span></div>
   <span id="praise" class="media_tool_meta meta_primary tips_global meta_praise"><i class="icon_praise_gray"></i><span class="praise_num">593</span></span>
   <span class="media_tool_meta tips_global meta_extra" href="javascript:void(0);">举报</span>
