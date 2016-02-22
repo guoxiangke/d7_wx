a9c5d8634cc7661e5bf67594e6f351bdf92ace21
dale <dale.guo@loweprofero.com>
2016-02-22 21:01:35 +0800

    prevnext

--- sites/all/themes/xdtheme/template.php
+++ sites/all/themes/xdtheme/template.php
@@ -81,3 +81,44 @@ function xdtheme_preprocess_comment_wrapper(&$variables) {
   // dpm($variables);
   $variables['theme_hook_suggestions'][] = 'comment__wrapper_custom_' . $variables['node']->type;
 }
+
+/**
+* Previous / Next function for nodes, ordered by node creation date
+*
+* @param $current_node: node object or node id
+* @param $node_types:  array of node types to query
+*
+* @return array
+*
+*/
+function dale_prev_next_node($current_node = NULL, $node_types = array()) {
+    // make node object if only node id given
+    if (!is_object($current_node)) { $current_node = node_load($current_node->nid); }
+
+    // make an array if string value was given
+    if (!is_array($node_types)) { $node_types = array($node_types); }
+
+    // previous
+    $prev = db_select('node', 'n')
+    ->fields('n',array('nid','title','created'))
+    ->condition('n.status', 1,'=')
+    ->condition('n.type', $node_types,'IN')
+    ->condition('n.created', $current_node->created,'<')
+    ->orderBy('created','DESC')
+    ->range(0,1)
+    ->execute()
+    ->fetchAssoc();
+
+    // next or false if none
+    $next = db_select('node', 'n')
+    ->fields('n',array('nid','title','created'))
+    ->condition('n.status', 1,'=')
+    ->condition('n.type', $node_types,'IN')
+    ->condition('n.created', $current_node->created,'>')
+    ->orderBy('created','ASC')
+    ->range(0,1)
+    ->execute()
+    ->fetchAssoc();
+
+    return array('prev' => $prev, 'next' => $next);
+}
