49fddb54d0cc3ceef64e5ec58bd01455bbd38cd4
dale <dale.guo@loweprofero.com>
2016-07-22 18:06:55 +0800

    location up

--- get_location.php
+++ get_location.php
@@ -1,12 +1,16 @@
-<?php
+<pre><?php
 function get_location($url=NULL){
 	if($url == NULL)
 		$url = $_GET["url"];
 	$heads = get_headers($url,TRUE);
+  // var_dump($heads);
 	$location = FALSE;
 	if(isset( $heads['location'] )){
 		$location = $heads['location'];
 	}
-	return $location;	
+  if(isset( $heads['Location'] )){
+    $location = $heads['Location'];
+  }
+	return $location;
 }
 echo get_location();
